0dc7622d15cbacfe2366bd0455a4d4380127cc0f
jj <log@riseup.net>
2025-11-23 14:02:35 +0000

    package/apprun: add missing license header

--- package/AppRun.sh
+++ package/AppRun.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Copyright 2025 The Helium Authors
+# You can use, redistribute, and/or modify this source code under
+# the terms of the GPL-3.0 license that can be found in the LICENSE file.
+
 THIS="$(readlink -f "${0}")"
 HERE="$(dirname "${THIS}")"
 export LD_LIBRARY_PATH="${HERE}/usr/lib:$LD_LIBRARY_PATH"
