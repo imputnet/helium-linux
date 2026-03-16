ca054fd2dd0b7c89c0e1b3892d6816c068aa8489
koyu <me@koyu.space>
2026-03-16 06:46:24 +0100

    Update release script to include version info in AppStream metadata

--- .github/bump-hook.sh
+++ .github/bump-hook.sh
@@ -4,7 +4,14 @@ set -euxo pipefail
 PLATFORM_DIR="$PWD"
 HELIUM_DIR="$PLATFORM_DIR/helium-chromium"
 SPEC="$PLATFORM_DIR/package/helium-bin.spec"
+METAINFO="$PLATFORM_DIR/package/metainfo/net.imput.helium.metainfo.xml"
 
 # version_after is exported by `bump-platform` action
 sed -Ei "s/^(%define version ).*/\1$version_after/" "$SPEC"
-git add -u "$SPEC"
+
+# Update AppStream metainfo.xml with new release version and date
+RELEASE_DATE=$(date +%Y-%m-%d)
+# Update the first release entry with new version and date
+sed -Ei "0,/<release version=\"[^\"]*\" date=\"[^\"]*\" \/>/s/<release version=\"[^\"]*\" date=\"[^\"]*\" \/>/<release version=\"$version_after\" date=\"$RELEASE_DATE\" \/>/" "$METAINFO"
+
+git add -u "$SPEC" "$METAINFO"
