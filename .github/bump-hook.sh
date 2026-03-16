29604fd75b2f5ce4a3071fd860ac99078935bd39
koyu <me@koyu.space>
2026-03-16 11:02:33 +0100

    Update .github/bump-hook.sh
    Co-authored-by: jj <log@riseup.net>

--- .github/bump-hook.sh
+++ .github/bump-hook.sh
@@ -10,7 +10,6 @@ METAINFO="$PLATFORM_DIR/package/metainfo/net.imput.helium.metainfo.xml"
 sed -Ei "s/^(%define version ).*/\1$version_after/" "$SPEC"
 
 RELEASE_DATE=$(date +%Y-%m-%d)
-# Update the first release entry with new version and date
 sed -Ei 's/(<release version=")(.*)(" date=")(.*)(" \/>)/'"\1$version_after\3$RELEASE_DATE\5/" "$METAINFO"
 
 git add -u "$SPEC" "$METAINFO"
