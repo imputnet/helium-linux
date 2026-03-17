e4b7706ed6ae48afad39e2edb29a31add062e937
koyu <me@koyu.space>
2026-03-17 10:40:37 +0100

    I hope this fixes Copr builds

--- .github/bump-hook.sh
+++ .github/bump-hook.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 PLATFORM_DIR="$PWD"
 HELIUM_DIR="$PLATFORM_DIR/helium-chromium"
 SPEC="$PLATFORM_DIR/package/helium-bin.spec"
-METAINFO="$PLATFORM_DIR/package/metainfo/net.imput.helium.metainfo.xml"
+METAINFO="$PLATFORM_DIR/package/net.imput.helium.metainfo.xml"
 
 # version_after is exported by `bump-platform` action
 sed -Ei "s/^(%define version ).*/\1$version_after/" "$SPEC"
