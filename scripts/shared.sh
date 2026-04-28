b8d10ba5c1a4515b69ab3fa1b0c16ca23a4a3948
Clickot <clickot3@gmail.com>
2026-04-28 13:14:11 +0200

    create symlink to system gperf to work around the intruduction of google's own gperf binary

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -134,6 +134,8 @@ setup_toolchain() {
 
     mkdir -p "${_src_dir}/third_party/node/linux/node-linux-x64/bin"
     ln -sf "$(which node)" "${_src_dir}/third_party/node/linux/node-linux-x64/bin/node"
+    mkdir -p "${_src_dir}/third_party/gperf/cipd/bin/"
+    ln -sf "$(which gperf)" "${_src_dir}/third_party/gperf/cipd/bin/gperf"
 
     local clang_bin="${_src_dir}/third_party/llvm-build/Release+Asserts/bin"
     export CC="${clang_bin}/clang"
