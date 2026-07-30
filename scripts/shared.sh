6d992ab00a930c428c3ca7b07b0357ff5cf4eb2a
jj <log@riseup.net>
2026-07-30 21:04:42 +0000

    scripts: add mkdir for mold

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -208,6 +208,7 @@ setup_toolchain() {
     ln -sf "$(which node)" "${_src_dir}/third_party/node/linux/node-linux-x64/bin/node"
     mkdir -p "${_src_dir}/third_party/gperf/cipd/bin/"
     ln -sf "$(which gperf)" "${_src_dir}/third_party/gperf/cipd/bin/gperf"
+    mkdir -p "${_src_dir}/buildtools/third_party/mold/cipd/"
     ln -sf "$(which mold)" "${_src_dir}/buildtools/third_party/mold/cipd/mold"
     mkdir -p "${_src_dir}/third_party/dawn/tools/golang/linux-amd64/bin"
     ln -sf "$(which go)" "${_src_dir}/third_party/dawn/tools/golang/linux-amd64/bin/go"
